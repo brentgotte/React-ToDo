7aba24ea0bfb44b13043dada0de72f4dd509f182
brentgotte <brent.gotte@gmail.com>
2022-12-02 12:20:13 +0100

    Remove Delete-message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Todo from './components/Todo';
-// import Delmsg from './components/Delmsg';
 function App() {
   return (
     <div>
       <h1>My toDo's</h1>
-      {/* <Delmsg/> */}
       <Todo text='learn react'/>
       <Todo text='master react'/>
       <Todo text='master react again'/>
